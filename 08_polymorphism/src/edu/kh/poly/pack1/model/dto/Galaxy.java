bba96afda9fd4eb448586c795da2eccede313231
OZ <macoto8@naver.com>
2024-02-07 09:50:11 +0900

    240207_Java
    다형성

---
+++ 08_polymorphism/src/edu/kh/poly/pack1/model/dto/Galaxy.java
@@ -0,0 +1,8 @@
+package edu.kh.poly.pack1.model.dto;
+
+public class Galaxy extends SmartPhone {
+	
+	// 안드로이드 운영체제 버전
+	private int androdVersion;
+
+}
---
+++ 08_polymorphism/src/edu/kh/poly/pack1/model/dto/Iphone.java
@@ -0,0 +1,8 @@
+package edu.kh.poly.pack1.model.dto;
+
+public class Iphone extends SmartPhone {
+
+	// IOS 운영체제 버전
+	private int iosVersion;
+	
+}
