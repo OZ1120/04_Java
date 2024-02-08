4f99f3bc37d1d039f849262ea4f7c09e2aa91134
OZ <macoto8@naver.com>
2024-02-08 17:03:01 +0900

    240208_Java
    제네릭스

--- 09_collection/src/edu/kh/collection/pack1/run/ListRun.java
+++ 09_collection/src/edu/kh/collection/pack1/run/ListRun.java
@@ -8,6 +8,13 @@ public class ListRun {
 		
 		ListService service = new ListService();
 		
-		service.method1();
+//		service.method1();
+//		service.method2();
+		
+		service.bookManage();
+		
+		
+		
+		
 	}
 }
