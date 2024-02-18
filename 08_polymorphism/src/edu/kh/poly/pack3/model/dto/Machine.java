64dca9df9210e95ddedcba9c951708a71a826a7f
DESKTOP-92TCAM3\lg <macoto8@naver.com>
2024-02-18 20:57:32 +0900

    240208_Java 인터페이스

--- 08_polymorphism/src/edu/kh/poly/pack3/model/dto/Machine.java
+++ 08_polymorphism/src/edu/kh/poly/pack3/model/dto/Machine.java
@@ -5,8 +5,8 @@ package edu.kh.poly.pack3.model.dto;
 
 public interface Machine {
 
-	// 인터페이스는 상수형 필드(public static fail)만 작성 가능!
-	// -> 작성하는 모든 필드가 public strtic fianl
+	// 인터페이스는 상수형 필드(public static final)만 작성 가능!
+	// -> 작성하는 모든 필드가 public strtic final
 	// ->public static final구문을 명시적으로 작성하지 않아도
 	//	묵시적으로 public static final로 인식된다!
 	
