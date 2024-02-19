04688910e0f4ffc8d50bea47977721ac98056cff
OZ <macoto8@naver.com>
2024-02-19 10:31:19 +0900

    240219_Java test

---
+++ test/src/edu/kh/test2/test2.java
@@ -0,0 +1,31 @@
+package edu.kh.test2;
+// 2월 19일 시험
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List<IPInfo> ipInfoList	 = new ArrayList<IPInfo>();
+		
+		ipInfoList.add(new IPInfo("123.123.123.123", "홍길동"));
+		ipInfoList.add(new IPInfo("212.133.7.8", "ㅇㅇㅈ"));
+		ipInfoList.add(new IPInfo("212.133.7.8", "고길순"));
+		
+		System.out.print("ip 입력 : ");
+		String ip = sc.next();
+		
+//		for(int i=0; i<ipInfoList.size(); i++) {
+//			if(ipInfoList.get(i) == ip) {
+//				System.out.println(ipInfoList.get(i));
+//				break;
+//			}
+//		}
+		System.out.println("일치하는 ip 사용자가 없습니다");
+	}
+}
